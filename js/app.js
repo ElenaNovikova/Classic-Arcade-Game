6ec08ee4a24f3d6fd966c4326cd299dc3c025e13
Elena Novikova <36162105+ElenaNovikova@users.noreply.github.com>
2018-06-30 15:34:56 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+// TODO: implement collection of gems, allowing the player to collect them during the crossing the road
+
 // Counter for game lives
-var score = 0;
-const scorePanel = document.getElementById('score_panel');
+var counterCollisions = 0;
+var score = document.querySelectorAll('#score');
 
 const Game = function() {
     //this.width = 505;
@@ -16,7 +18,7 @@ var Enemy = function(x, y, speed) {
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
-    this.speed = Math.floor(Math.random() * 280) + 40;
+    this.speed = Math.floor(Math.random() * 580) + 300;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-car.png';
@@ -27,9 +29,17 @@ function shakingC() {
     setTimeout(function() {
         //console.log('You lost!')
         canvas.classList.remove('collisionShake');
-    }, 1000);
+    }, 900);
     //return;
     //window.clearInterval();
+    counterCollisions++;
+    console.log(counterCollisions);
+    score.forEach(function(fas) {
+		    fas.innerHTML = '<i class="fas fa-heart" style="color: black"></i>';
+	  });
+    if (counterCollisions == 5) {
+        gameOver();
+    }
 }
 
 // Update the enemy's position, required method for game
@@ -42,14 +52,19 @@ Enemy.prototype.update = function(dt) {
         this.x += this.speed * dt;
     } else {
         this.x = -50; // reseting the position of the enemy, when it goes off the canvas
-    };
+    }
     // Collision detection between the player and the enemies
     if (player.x < this.x + 75 && player.x + 75 > this.x &&
         player.y < this.y + 55 && 55 + player.y > this.y) {
         player.x = 203;
         player.y = 295;
         shakingC();
-    };
+    }
+    // Player wins the game, when reaches the opposite sidewalk
+    // Then Modale window with congrats appears
+    if (player.x <= 505 && player.y < 50) {
+        playerWins();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -65,7 +80,7 @@ const Player = function() {
     this.x = game.playerStartX;
     this.y = game.playerStartY;
     this.move = [0, 0];
-    this.live = 3;
+    //this.live = 3;
     this.end = false;
     this.sprite = 'images/char-horn-girl.png';
 };
@@ -114,43 +129,72 @@ const allEnemies = [
   new Enemy(200,60)
 ];
 
-//Player wins the game when reaches the opposite sidewalk
-// Modale window with congrats appears
 function playerWins() {
-    // something happens when player wins
+    // If the Player won, the canvas is cleared from the enemies
+    allEnemies.splice(0);
+    document.removeEventListener('keyup', keyupELis);
+    // You won! Modal window appears
+    modal.style.display = "block";
 }
 
-//Setting up the Modal popup window:
-// Get the modal:
+function gameOver() {
+    // If the Player lost, the game stops
+    allEnemies.forEach(function(enemy) {
+		    enemy.speed = 0;
+	  });
+    document.removeEventListener('keyup', keyupELis);
+    // Game Over! Modal window appears
+    modalNumTwo.style.display = "block";
+}
+
+/*
+ * Setting up the Modal popup windows
+ */
+
+// Get the modal
 let modal = document.getElementById('myModal');
+let modalNumTwo = document.getElementById('myModalTwo');
 
-// Get the <span> element that closes the modal:
+// Get the <span> element that closes the modal
 let span = document.getElementsByClassName('close')[0];
+let span2 = document.getElementsByClassName('close')[1];
 
-// When the user clicks on <span> (x), close the modal:
+// When the user clicks on <span> (x), close 1st modal
 span.onclick = function() {
     modal.style.display = 'none';
 }
 
-// When the user clicks anywhere outside of the modal, close it:
+// When the user clicks on <span> (x), close 2nd modal
+span2.onclick = function() {
+    modalNumTwo.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of 1st modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = 'none';
     }
 }
 
+// When the user clicks anywhere outside of 2nd modal, close it
+window.addEventListener("click", function(event) {
+    if (event.target == modalNumTwo) {
+        modalNumTwo.style.display = 'none';
+    }
+});
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', keyupELis);
+function keyupELis(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
-});
+}
 
 // Restarting the game after click on the Restart button
 let restartGameBtn = document.getElementById('restart_btn');
@@ -158,3 +202,7 @@ restartGameBtn.addEventListener('click', function() {
     location.reload(true);
     //document.location.href = '';
 });
+
+/*document.addEventListener('keydown', function(e) {
+    e.preventDefault();
+});*/
